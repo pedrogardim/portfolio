664c3ea86bf3701a4a0d3df1bfd11e467f6a208e
Pedro Gardim <me@pedrogardim.com>
2023-10-29 18:27:52 +0100

    feat: updated projects info

--- src/data/index.js
+++ src/data/index.js
@@ -1,35 +1,35 @@
 export const links = {};
 
 export const wordColorMap = {
-  darkBlue: ['const', 'new'],
-  blue: ['pedroGardim', 'i'],
+  darkBlue: ["const", "new"],
+  blue: ["pedroGardim", "i"],
   yellow: [
-    'hire',
-    '16beats',
-    'modulab',
-    'bless',
-    'kat',
-    'webDaw',
-    'metrono',
-    'pumblr',
-    'musabeat',
-    'leukos',
-    'gitHub',
+    "hire",
+    "16beats",
+    "modulab",
+    "bless",
+    "kat",
+    "webDaw",
+    "metrono",
+    "pumblr",
+    "musabeat",
+    "leukos",
+    "gitHub",
   ],
-  green: ['Date'],
-  lightBlue: ['now'],
+  green: ["Date"],
+  lightBlue: ["now"],
   salmon: [`'`],
 };
 
 export const colorPalette = {
-  yellow: '#DCDCAA',
-  darkBlue: '#569cd6',
-  lightBlue: '#9CDCFE',
-  blue: '#4FC1FF',
-  green: '#4EC9B0',
-  lime: '#b5cea8',
-  salmon: '#ce9178',
-  gray: '#777',
+  yellow: "#DCDCAA",
+  darkBlue: "#569cd6",
+  lightBlue: "#9CDCFE",
+  blue: "#4FC1FF",
+  green: "#4EC9B0",
+  lime: "#b5cea8",
+  salmon: "#ce9178",
+  gray: "#777",
 };
 
 export const colorMap = {};
@@ -42,63 +42,74 @@ Object.entries(wordColorMap).map((color) =>
 
 export const projects = [
   {
-    title: 'Bless',
-    picture: '../assets/images/projects/bless.png',
+    title: "Agape",
+    picture: "../assets/images/projects/agape.png",
     description:
-      'A dating app intended for Christians. It has a card swiping system, which matches users based on their preferences. Also features a complete chat and videocall system',
-    url: 'https://play.google.com/store/apps/details?id=io.virtualcase.bless',
+      "An innovative Meet & Connect App, which allows users to meet people with similar interests. Built with React Native (Expo), Redux TK, Tailwind,  Node.js (Fastify) and MongoDB.",
+    url: "https://agape-app.com/",
   },
   {
-    title: 'Modulab',
-    picture: '../assets/images/projects/modulab.png',
-    url: 'https://modulab-ee0e0.web.app',
+    title: "Modulab",
+    picture: "../assets/images/projects/modulab.png",
+    url: "https://github.com/pedrogardim/modulab",
     description:
-      'Simulation of modular syntesis on the web. Combine different audio processing modules to create unique sounds.',
+      "Simulation of modular syntesis on the web. Combine different audio processing modules to create unique sounds.",
   },
   {
-    title: 'Musabeat',
-    picture: '../assets/images/projects/musabeat.png',
+    title: "Musabeat",
+    picture:
+      "https://user-images.githubusercontent.com/81443264/224987137-e1a61977-9b35-4c13-b9a2-88ca67447c9c.gif",
     description:
-      'A collaborative music creation web app, which combines most features of a DAW (Digital Audio Workstation), with social network aspects',
-    url: 'https://github.com/pedrogardim/musabeat',
+      "A collaborative music creation web app, which combines most features of a DAW (Digital Audio Workstation), with social network aspects",
+    url: "https://github.com/pedrogardim/musabeat",
   },
   {
-    title: 'Leukos',
-    picture: '../assets/images/projects/leukos.png',
+    title: "GameboyJS",
+    picture:
+      "https://user-images.githubusercontent.com/81443264/269696469-3289bf5a-41e7-4823-9bd7-fe8c459ee238.gif",
     description:
-      'Songbook system for liturgical music. The mobile app features chord transpose and song playback, while the desktop app will project the lyrics',
+      "Songbook system for liturgical music. The mobile app features chord transpose and song playback, while the desktop app will project the lyrics",
+    url: "https://github.com/pedrogardim/gameboyjs",
   },
   {
-    title: 'Petr',
-    picture: '../assets/images/projects/petr.png',
-    description: 'A virtual pet on the web',
+    title: "Mastermind",
+    picture:
+      "https://user-images.githubusercontent.com/81443264/271700137-2aded648-1aa6-419c-9938-ff447aa40d03.gif",
+    description:
+      "Web adaptation of the classic code-breaking board game Mastermind",
+    url: "https://github.com/pedrogardim/mastermind",
   },
   {
-    title: 'Web DAW',
-    picture: '../assets/images/projects/webdaw.png',
-    description: 'A ',
-    url: 'https://github.com/pedrogardim/daw_prototype',
+    title: "Web DAW",
+    picture: "../assets/images/projects/webdaw.png",
+    description: "My first experiment with Web Audio API, a music creation app",
+    url: "https://github.com/pedrogardim/daw_prototype",
   },
   {
-    title: 'Metrono',
-    picture: '../assets/images/projects/metrono.png',
-    description: 'Web metronome with a minimalist design',
+    title: "Coffea Co Landing Page",
+    picture:
+      "https://user-images.githubusercontent.com/81443264/268409367-a3d7f006-e88c-4c65-8a47-e29b9827b7be.gif",
+    description:
+      "Welcome to the Coffea Co landing page! This project was completed as Project 1 of the Full Stack Developer Bootcamp at Geekshubs Academy.",
+    url: "https://github.com/pedrogardim/bcfs-coffea-landing-page/",
   },
   {
-    title: 'Plumb',
-    picture: '../assets/images/projects/pumblr.png',
-    description: 'Clone of the classic game using JS',
-    url: 'https://github.com/pedrogardim/plumb',
+    title: "Scriptura API",
+    picture:
+      "https://images.pexels.com/photos/3861976/pexels-photo-3861976.jpeg?cs=srgb&dl=pexels-thisisengineering-3861976.jpg&fm=jpg&w=640&h=960&_gl=1*14j4rc9*_ga*OTAwNTgzNzYxLjE2ODg2Nzg2NDA.*_ga_8JE65Q40S6*MTY5ODU5OTY4MC42LjEuMTY5ODU5OTk0NC4wLjAuMA..",
+    description:
+      "RESTful API offering data on biblical characters, places, verses, and events. Based on Golang and PostgreSQL.",
+    url: "https://github.com/pedrogardim/scriptura-api",
   },
 ];
 
 export const education = [
-  { title: 'Bless' },
-  { title: 'Leukos' },
-  { title: 'Modulab' },
-  { title: 'Musabeat' },
-  { title: 'Kat' },
-  { title: 'Web DAW' },
-  { title: 'Metrono' },
-  { title: 'Pumblr' },
+  { title: "Bless" },
+  { title: "Leukos" },
+  { title: "Modulab" },
+  { title: "Musabeat" },
+  { title: "Kat" },
+  { title: "Web DAW" },
+  { title: "Metrono" },
+  { title: "Pumblr" },
 ];
